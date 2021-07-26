46520dee9f26767b55665112bd73da00ad586c2e
errk01 <erik.taylor30@gmail.com>
2021-07-26 10:11:20 -0700

    Twitter day one

--- WeekSix/E-commerce/src/main/java/com/example/Ecommerce/securityConfig/SecurityConfiguration.java
+++ WeekSix/E-commerce/src/main/java/com/example/Ecommerce/securityConfig/SecurityConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -30,7 +31,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception{
         http
                 .authorizeRequests()
-                .antMatchers("/console").permitAll()
+                .antMatchers("/console/**").permitAll()
                 .antMatchers("/cart").authenticated()
                 .and().formLogin()
                 .loginPage("/signin")
@@ -39,7 +40,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .logoutRequestMatcher(new AntPathRequestMatcher("/signout"))
                 .logoutSuccessUrl("/");
     }
-
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+                .ignoring()
+                .antMatchers("/console/**");
+    }
 
 
 }
